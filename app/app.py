ca6d9babb941d7a2eb9a427582b72f826ee6d3d1
Kirill Trofimov <crazytraffic@gmail.com>
2019-08-23 13:01:53 +0300

    +patch validate, +patch relatives properly update

--- app/app.py
+++ app/app.py
@@ -53,20 +53,41 @@ def change_citizen_info(import_id, citizen_id):
     if import_table not in DB.list_collection_names():
         abort(404)
     
+    current = DB[import_table].find_one({"citizen_id" : citizen_id})
+    if not current:
+        abort(404)
+
     data = request.json
     try:
         validate_patch(data)
+        # validate relatives
+        if 'relatives' in data:
+            # cant be own relative
+            assert citizen_id not in data['relatives']
+
+            # dublicate check
+            assert len(data['relatives']) == len(set(data['relatives']))
+
+            #bad citizen_id in relatives check
+            for relative in data['relatives']:
+                assert DB[import_table].find({"citizen_id" : relative})
     except:
         abort(400)
 
-    query = { "citizen_id" : citizen_id}
-    values = { "$set" : data}
-    result = DB[import_table].find_one_and_update(query, values, return_document=ReturnDocument.AFTER)
+    if 'relatives' in data:
+        # delete old relatives
+        for relative in current['relatives']:
+            DB[import_table].find_one_and_update({"citizen_id" : relative}, {"$pull" : citizen_id})
+        # add new relatives
+        for relative in data['relatives']:
+            DB[import_table].find_one_and_update({"citizen_id" : relative}, {"$addToSet" : citizen_id})
 
-    if len(result) > 0:
+    #patch
+    result = DB[import_table].find_one_and_update({ "citizen_id" : citizen_id}, { "$set" : data}, return_document=ReturnDocument.AFTER)
+    if result:
         return result_wrapper(result), 200
     else:
-        abort(404)
+        abort(500)
 
 @app.route('/imports/<string:import_id>/citizens', methods=['GET'])
 def get_citizens(import_id):
