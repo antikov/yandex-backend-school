6f7305631b7e24a382525f9fb65a43386ffd0740
Kirill Trofimov <crazytraffic@gmail.com>
2019-09-30 02:44:31 +0300

    added prints

--- homework1/task_01.py
+++ homework1/task_01.py
@@ -9,7 +9,8 @@ from datetime import datetime
 years = [1967, 1978, 2000, 1987, 2010, 1977]
 current_year = datetime.now().year
 answer = [current_year - year for year in years]
-
+print(answer)
 
 #one line solution
 answer = [2019 - year for year in [1967, 1978, 2000, 1987, 2010, 1977]]
+print(answer)
